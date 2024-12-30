f4a8ab7dc564d4240d70cbbf90288af6794e09be
martinarielriveros <martin.riveros@hotmail.com>
2024-12-30 10:41:48 -0300

    fix error in variable name

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,6 +3,6 @@ import { get } from "env-var";
 
 export const envs = {
   // Port number for the environment variable that is used to connect to the server  (default is 3000)
-  server_port: get("PORT").default(3000).required().asPortNumber(),
+  server_port: get("SERVER_PORT").default(3000).required().asPortNumber(),
   public_path: get("PUBLIC_PATH").default("public").asString(),
 };
